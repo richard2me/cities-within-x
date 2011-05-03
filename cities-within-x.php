195f7d86006666658ed8a9e582fc7825a1eaf329
Richard Twomey <richard2me@gmail.com>
2011-05-03 04:12:49 -0700

    second commit is the second commit

---
+++ cities-within-x.php
@@ -0,0 +1,40 @@
+<?php
+
+/*
+Cities Within X
+Find zip codes within x miles of a given zip code
+
+This script assumes that you have an SQL table
+called "zip_coords", which contains these columns:
+- zipcode
+- latitude
+- longitude
+
+*/
+
+$zip = 94550; // "find nearby this zip code"
+$radius = 15; // "search radius (miles)"
+$maxresults = 10; // maximum number of results you'd like
+
+$sql = "SELECT * FROM
+	(SELECT o.zipcode, o.city, o.state,
+		(3956 * (2 * ASIN(SQRT(
+		POWER(SIN(((z.latitude-o.latitude)*0.017453293)/2),2) +
+		COS(z.latitude*0.017453293) *
+		COS(o.latitude*0.017453293) *
+		POWER(SIN(((z.longitude-o.longitude)*0.017453293)/2),2)
+		)))) AS distance
+	FROM zip_coords z,
+		zip_coords o,
+		zip_coords a
+	WHERE z.zipcode = ".(int)$zip." AND z.zipcode = a.zipcode AND
+		(3956 * (2 * ASIN(SQRT(
+		POWER(SIN(((z.latitude-o.latitude)*0.017453293)/2),2) +
+		COS(z.latitude*0.017453293) *
+		COS(o.latitude*0.017453293) *
+		POWER(SIN(((z.longitude-o.longitude)*0.017453293)/2),2)
+		)))) <= ".(int)$radius."
+	ORDER BY distance)
+	ORDER BY distance ASC LIMIT 0,".(int)$maxresults;
+
+?>
